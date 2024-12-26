b54ac99111ab8de1b72d6ecb2b2c0f4a7db6546a
Sigureya <sigureya1208@gmail.com>
2024-12-26 16:00:45 +0900

    ライブラリを仮ビルド。

--- dist/railtoy.es.js
+++ dist/railtoy.es.js
@@ -1,10 +1,19 @@
-const s = (t) => t % 36e4, r = (t, e) => s(t.angle + e.angle) === 0, a = (t, e) => t.jointType + e.jointType === 0, o = (t, e) => t.trackOffset === e.trackOffset, f = (t, e) => t.height === e.height, c = (t, e) => t.stationOffset === e.stationOffset, i = (t, e) => o(t, e) && c(t, e) && a(t, e) && r(t, e), n = (t) => ({
-  angle: 0,
-  stationOffset: 0,
-  trackOffset: 0,
-  height: 0,
-  ...t
-}), l = () => n({ jointType: 1 }), p = () => n({ jointType: -1 });
+const s = (t) => t % 36e4,
+  r = (t, e) => s(t.angle + e.angle) === 0,
+  a = (t, e) => t.jointType + e.jointType === 0,
+  o = (t, e) => t.trackOffset === e.trackOffset,
+  f = (t, e) => t.height === e.height,
+  c = (t, e) => t.stationOffset === e.stationOffset,
+  i = (t, e) => o(t, e) && c(t, e) && a(t, e) && r(t, e),
+  n = (t) => ({
+    angle: 0,
+    stationOffset: 0,
+    trackOffset: 0,
+    height: 0,
+    ...t,
+  }),
+  l = () => n({ jointType: 1 }),
+  p = () => n({ jointType: -1 });
 export {
   r as areAnglesCompatible,
   f as areHeightCompatible,
@@ -15,5 +24,5 @@ export {
   n as joint,
   p as minus,
   s as normalizeAngle,
-  l as plus
+  l as plus,
 };
