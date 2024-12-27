be6de84b53279c18f4526bfe36040480095a2f3d
Sigureya <sigureya1208@gmail.com>
2024-12-28 05:31:13 +0900

    eslintの設定を変更。動かなかったのはバージョン問題らしい

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,19 @@
 module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  settings: {
-    react: {
-      version: "detect",
-    },
-  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn",
     "@typescript-eslint/no-unused-vars": "warn",
     "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/consistent-type-imports": "error",
+    "@typescript-eslint/consistent-type-imports": [
+      "warn",
+      {
+        prefer: "type-imports",
+      },
+    ],
+
     "spaced-comment": "error",
   },
 };
