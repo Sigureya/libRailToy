8e3760018753f03eba01ccc0241a898cb9474128
Sigureya <sigureya1208@gmail.com>
2024-12-28 23:55:14 +0900

    テストを更新

--- src/rails/types/vectorFromShape.test.ts
+++ src/rails/types/vectorFromShape.test.ts
@@ -1,10 +1,21 @@
 import { describe, expect, test } from "vitest";
 import { ShapeFactory } from "./shape";
+import { vectorFromRailShape } from "./vectorFromShape";
+import { add, scale, zeroVector } from "./vector4";
 
 describe("shapeのテスト", () => {
   const factory = new ShapeFactory();
-  const mockCurve = factory.curve(1);
-  test("", () => {
-    expect(true).toBeTruthy();
+
+  const vectorCurve0 = vectorFromRailShape(factory.curve(0));
+  const vectorCurve45 = vectorFromRailShape(factory.curve(1));
+  const vectorCurve90 = vectorFromRailShape(factory.curve(2));
+
+  test("0度の曲線はゼロベクトルか?", () => {
+    expect(vectorCurve0).toEqual(zeroVector());
+  });
+
+  test("曲線レールの足し算", () => {
+    expect(add(vectorCurve45, vectorCurve45)).toEqual(vectorCurve90);
+    expect(scale(vectorCurve45, 2)).toEqual(vectorCurve90);
   });
 });
