751720e89e03938f20e98d6c6495d2d04f090479
Sigureya <sigureya1208@gmail.com>
2024-12-30 18:30:10 +0900

    変数にカーソルを当てたときに表示されて邪魔なので削除 管理方法はおいおい考える

--- src/rails/types/vector4/newCode/identityConstants.ts
+++ src/rails/types/vector4/newCode/identityConstants.ts
@@ -1,9 +1,6 @@
 import type { UnitRailVector4 } from "../railVector4";
 
 type Vec4 = UnitRailVector4;
-/**
- * @description 8方向の専用角度を定義
- */
 export const VECTOR_ANGLE_0: Readonly<Vec4> = { a: 1, b: 0, c: 0, d: 0 }; // 真上
 export const VECTOR_ANGLE_1: Readonly<Vec4> = { a: 0, b: 1, c: 0, d: 0 }; // 右上
 export const VECTOR_ANGLE_2: Readonly<Vec4> = { a: 0, b: 0, c: 1, d: 0 }; // 右
