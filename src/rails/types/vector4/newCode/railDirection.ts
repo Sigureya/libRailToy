ed9cb102805bea4a4e14f6c81959d2577ffa3dad
Sigureya <sigureya1208@gmail.com>
2024-12-30 14:10:25 +0900

    テーブル自体をクラスにした。以前の実装がstaticな変数＋関数だったので、こっちに差し替える方が良さそう

---
+++ src/rails/types/vector4/newCode/railDirection.ts
@@ -0,0 +1,20 @@
+import type { DirectionalUnitVectors, UnitRailVector4 } from "../railVector4";
+import type { TableEntry } from "./createTable";
+import { createAngleTable } from "./createTable";
+import type { CyclicArray } from "./cyclicArray";
+
+export class RailAngleTable {
+  private _table: CyclicArray<TableEntry>;
+  constructor(table: CyclicArray<TableEntry> = createAngleTable()) {
+    this._table = table;
+  }
+  straight(
+    angle: number,
+    direction: keyof DirectionalUnitVectors = "forward"
+  ): UnitRailVector4 {
+    return this._table.at(angle).STRAIGHT[direction];
+  }
+  curve(angle: number, arc: number) {
+    return this._table.at(angle).CURVE.at(arc);
+  }
+}
