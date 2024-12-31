9ee74d4811a36589a0bd819ae425198354e9907e
Sigureya <sigureya1208@gmail.com>
2024-12-31 23:09:15 +0900

    レールの単位系に関するテストを追加

--- src/rails/types/shape/shapeFactory.test.ts
+++ src/rails/types/shape/shapeFactory.test.ts
@@ -14,3 +14,18 @@ describe("レール製造テスト", () => {
     expect(MockShapeFactory.curve(2)).toEqual(MockCurve90);
   });
 });
+describe("単位系が一致するか？", () => {
+  test("曲線半径と直線の長さは一致する", () => {
+    const curve = MockShapeFactory.curve(1);
+    const straight = MockShapeFactory.straight();
+    expect(curve.curveLength).toEqual(straight.straightLength);
+  });
+  test("直線の単位系が一致する", () => {
+    const half = MockShapeFactory.straightHalf();
+    const straight = MockShapeFactory.straight();
+    const quarter = MockShapeFactory.straightQuarter();
+    expect(half.straightLength * 2).toBe(straight.straightLength);
+    expect(quarter.straightLength * 4).toBe(straight.straightLength);
+    expect(quarter.straightLength * 2).toBe(half.straightLength);
+  });
+});
