eab6789b51c4ed7df35456d1cc8a1d95e93fc910
Sigureya <sigureya1208@gmail.com>
2024-12-31 12:07:29 +0900

    次の座標を求める関数。レールの全座標を求める時に必要となる

--- src/rails/types/accumlateVector.ts
+++ src/rails/types/accumlateVector.ts
@@ -1,6 +1,10 @@
 import type { RailShape } from "./shape";
+import type { RailVector4 } from "./vector4";
 import { zeroVector } from "./vector4";
-import type { RailTransform } from "./vector4/railTransfrom";
+import type {
+  RailTransform,
+  ReadonlyRailTransform,
+} from "./vector4/railTransfrom";
 import { vectorFromRailShape } from "./vectorFromShape";
 
 export const accmulateVector = (
@@ -23,4 +27,29 @@ export const accmulateVector = (
   }
 
   return result;
+};
+
+export const nextPosition = (
+  prevPosition: ReadonlyRailTransform,
+  shape: RailShape
+): RailTransform => {
+  return {
+    angle: prevPosition.angle + shape.arc,
+    movement: vectorFromRailShape(shape, prevPosition.angle),
+  };
+};
+
+const mapVector = (shapes: ReadonlyArray<Readonly<RailShape>>, angle = 0) => {
+  let pre: { readonly angle: number; movement: Readonly<RailVector4> } = {
+    angle: angle,
+    movement: zeroVector(),
+  };
+  return shapes.map<RailTransform>((shape) => {
+    const vector = vectorFromRailShape(shape, pre.angle);
+    pre = {
+      angle: shape.arc + pre.angle,
+      movement: vector,
+    };
+    return pre;
+  });
 };
