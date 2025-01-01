03f41eb02d8b7a9e11bd0a288a9480d4e6b87120
Sigureya <sigureya1208@gmail.com>
2025-01-01 14:06:15 +0900

    モックのファイル名を変更。

--- src/rails/types/shape/mockShape.test.ts
+++ src/rails/types/shape/mockShape.test.ts
@@ -1,5 +1,5 @@
 import { test, expect, describe } from "vitest";
-import { MockStraight, MockStraightLong } from "./mock";
+import { MockStraight, MockStraightLong } from "./mockShape";
 
 test("2倍直線レールは直線レールの2倍", () => {
   expect(MockStraight.straightLength * 2).toBe(MockStraightLong.straightLength);
