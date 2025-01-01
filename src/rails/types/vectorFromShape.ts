00bb02385f9b0fb660d462bdaa22508bc5d2a3f2
Sigureya <sigureya1208@gmail.com>
2025-01-01 22:07:07 +0900

    コメントを修正

--- src/rails/types/vectorFromShape.ts
+++ src/rails/types/vectorFromShape.ts
@@ -3,23 +3,37 @@ import type { RailShape } from "./shape";
 import type { RailVector4 } from "./vector4";
 import { add, scale } from "./vector4";
 
-// とりあえず直線成分だけ取得してみる
-export const straightVector = (shape: RailShape, angle = 0): RailVector4 => {
+/**
+ * @description レールの直線部分の形状を取得する
+ * @param shape レールの形状
+ * @param starAngle 開始地点の角度
+ * @returns
+ */
+export const straightVector = (
+  shape: RailShape,
+  starAngle = 0
+): RailVector4 => {
   // 曲線側を除算する代わりに直線を2倍にする
-  return scale(ANGLE_TABLE.straight(angle), shape.straightLength * 2);
+  return scale(ANGLE_TABLE.straight(starAngle), shape.straightLength * 2);
 };
 
-export const curveVector = (shape: RailShape, angle = 0) => {
-  const begin = ANGLE_TABLE.straight(angle);
-  const end = ANGLE_TABLE.straight(angle + shape.arc);
+/**
+ * @description 形状から曲線部分の移動量をベクトルで取得する
+ * @param shape レールの形状
+ * @param startAngle 開始地点の角度
+ * @returns
+ */
+export const curveVector = (shape: RailShape, startAngle = 0) => {
+  const begin = ANGLE_TABLE.straight(startAngle);
+  const end = ANGLE_TABLE.straight(startAngle + shape.arc);
   const acm = add(begin, end);
   return scale(acm, shape.curveLength * Math.abs(shape.arc));
 };
-export const vectorFromRailShape = (shape: RailShape, angle = 0) =>
-  add(straightVector(shape, angle), curveVector(shape, angle));
-// railShape[]=>railNodeが必要
-// まず表向きのレールだけ考える
-
-// ベクトルを合成する必要があるので、 ループで対処する。
-// そしてループ処理は重たいので、テーブルで持つ
-// テスト用にテーブル実装を並行して用意する
+/**
+ * @description レールの移動量をベクトルで取得する
+ * @param shape レールの形状
+ * @param startAngle 開始地点の角度
+ * @returns
+ */
+export const vectorFromRailShape = (shape: RailShape, startAngle = 0) =>
+  add(straightVector(shape, startAngle), curveVector(shape, startAngle));
